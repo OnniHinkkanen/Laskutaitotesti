9508e9a85959cf93d0182463cf707a928f1a60b5
Hinkkanen <onukilla@jyu.fi>
2023-10-25 15:20:59 +0300

    jar fix

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ REM java -jar %0\laskutaitotesti.jar
 cls
 @pushd %~dp0
 :::::::::::::::::::
-java -jar laskutaitotesti.jar -pdf -tests 50 --maxima-path C:\devel\maxima-5.41.0\bin
+java -jar laskutaitotesti.jar -pdf -tests 50 --maxima-path C:\devel\maxima-5.41.0\bin\
 :::::::::::::::::::
 @popd
 pause
