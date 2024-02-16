9140d010cdac233062c9495660095f9cb7f2110c
Onni Hinkkanen <onni.u.i.hinkkanen@gmail.com>
2024-02-16 09:07:40 +0200

    vscode intgration

--- src/laskutaitotesti/Main.java
+++ src/laskutaitotesti/Main.java
@@ -9,8 +9,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.InputStream;
-import java.util.function.Consumer;
 import java.io.PrintStream;
 import java.math.BigDecimal;
 import java.nio.file.Files;
